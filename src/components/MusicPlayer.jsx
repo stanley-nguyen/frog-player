21ea99f19a68a7569eb2a68142e31d7c94cb7c36
stanley-nguyen <stanley.nguyen@sjsu.edu>
2024-07-20 18:14:33 -0700

    add functional play/pause button and display song time

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,9 +1,18 @@
-import { useRef, useContext, useEffect } from 'react';
+import { useRef, useContext, useEffect, useState } from 'react';
 import DirectoryContext from '../context/DirectoryContext';
+import { FaRegPlayCircle, FaRegPauseCircle } from "react-icons/fa";
 import './MusicPlayer.css';
+
+const secToTime = (totalSeconds) => {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+  return `${minutes}:${String(seconds).padStart(2, '0')}`;
+};
 
 function MusicPlayer() {
   const { isPlaying, setIsPlaying, currentSong, setCurrentSong } = useContext(DirectoryContext);
+  const [currentDuration, setCurrentDuration] = useState(null);
+  const [totalDuration, setTotalDuration] = useState(null);
 
   const audioElement = useRef(null);
 
@@ -27,11 +36,29 @@ function MusicPlayer() {
     }
   }, [isPlaying]);
 
+  const onPlaying = () => {
+    const duration = audioElement.current.duration;
+    const currentTime = audioElement.current.currentTime;
+
+    setCurrentDuration(currentTime);
+    setTotalDuration(duration);
+  }
+
   return (
-    <div className='player-controls'>
-      <button className='player-controls-toggle' onClick={() => setIsPlaying(!isPlaying)}>Play</button>
-      <audio ref={audioElement}></audio>
-    </div>
+    <>
+      <div className='player-controls'>
+        {isPlaying ? <FaRegPauseCircle className='player-controls-toggle' onClick={() => setIsPlaying(!isPlaying)} size={40}/> :
+                    <FaRegPlayCircle className='player-controls-toggle' onClick={() => setIsPlaying(!isPlaying)} size={40}/>}
+      </div>
+      <div className='progress-wrapper'>
+        <div className='player-controls-progress-bar' style={{width: `${currentDuration / totalDuration * 100 +"%"}`}}></div>
+      </div>
+      <audio ref={audioElement} onTimeUpdate={onPlaying}></audio>
+      <div className='player-controls-time'>
+        {currentDuration ? <span>{secToTime(currentDuration)} / {secToTime(totalDuration)}</span> :
+                           <></>}
+      </div>
+    </>
   )
 }
 
